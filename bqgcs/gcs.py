09e43cf28054c8a3858ac90aae9c0c730d1c5eb5
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2018-01-23 16:34:30 +0900

    feat: add job_config option

--- bqgcs/gcs.py
+++ bqgcs/gcs.py
@@ -3,11 +3,14 @@ from google.cloud import bigquery
 from .misc import gcs_path
 
 if False:
+    from typing import *
+
     from google.cloud import storage
 
 
-def gcs_to_bq(blob, table):  # type: (storage.Blob, bigquery.TableReference) -> None
+def gcs_to_bq(blob, table, job_config=None):
+    # type: (storage.Blob, bigquery.TableReference, Optional[bigquery.LoadJobConfig]) -> None
     bq = bigquery.Client()
 
-    job = bq.load_table_from_uri(gcs_path(blob), table)
+    job = bq.load_table_from_uri(gcs_path(blob), table, job_config=job_config)
     job.result()
